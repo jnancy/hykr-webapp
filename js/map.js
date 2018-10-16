c945a43ad8dec0bf6d742782e193fd8050aae272
Nancy Jiang <nancyjiang10@gmail.com>
2018-10-16 01:00:11 -0700

    done demo

--- js/map.js
+++ js/map.js
@@ -2,12 +2,33 @@ var map, mylat, mylng;
 var imageVar = 'row-image-';
 var titleVar = 'row-title-';
 var descriptionVar = 'row-description-';
-var infowindow = new google.maps.InfoWindow();
 
 console.log("Loading Map...")
 
 function initMap() {
 	var infowindow = new google.maps.InfoWindow();
+
+	function placeMarker(trail){
+		var marker = new google.maps.Marker({
+			position: {lat: trail.latitude, lng: trail.longitude},
+			map: map,
+			icon: 'img/mountain.png'
+		});
+		marker.addListener('click', function(){
+			infowindow.setContent( "<div id='infowindow'>"+ "<p>"+trail.name+"</p>"+ "Longitude: " + trail.longitude + "<div></div>" + "Latitude: " + trail.latitude +"</div>");
+			infowindow.open(map, this);
+		});
+	}
+
+	function newTrail(trail,i){
+		var indexNumber = i+1;
+		document.getElementById('row-image-'+indexNumber).src = trail.imgMedium;
+		document.getElementById('row-image-'+indexNumber).style.height = 'auto';
+		document.getElementById('row-image-'+indexNumber).style.width = '100vh';
+		document.getElementById('row-title-'+indexNumber).textContent = trail.name;
+		document.getElementById('row-description-'+indexNumber).textContent = trail.summary;
+	}
+
 	navigator.geolocation.getCurrentPosition(function (position) {
 		console.log(position.coords.latitude, position.coords.longitude);
 		mylat = position.coords.latitude;
@@ -37,16 +58,7 @@ function initMap() {
 						console.log(data);
 						for (var i = 0; i < data.trails.length; i++){
 							placeMarker(data.trails[i]);
-						}
-						for (var i = 0; i < 6; i++) {
-							var trail = data.trails[i];
-							console.log(trail);
-							var indexNumber = i+1;
-							document.getElementById('row-image-'+indexNumber).src = trail.imgMedium;
-							document.getElementById('row-image-'+indexNumber).style.height = 'auto';
-							document.getElementById('row-image-'+indexNumber).style.width = '100vh';
-							document.getElementById('row-title-'+indexNumber).textContent = trail.name;
-							document.getElementById('row-description-'+indexNumber).textContent = trail.summary;
+							if(i<6)newTrail(data.trails[i],i);
 						}
 					});
 				}
@@ -57,23 +69,189 @@ function initMap() {
 	});
 }
 
-function placeMarker(trail){
-	var marker = new google.maps.Marker({
-		position: {lat: trail.latitude, lng: trail.longitude},
-		map: map,
-		icon: 'img/mountain.png'
-	});
-	google.maps.event.addListener(marker, 'click', function(){
-		infowindow.setContent( "<div id='infowindow'>"+ trail.name +"</div>");
-		infowindow.open(map, this);
-	});
-}
-
-function newTrail(trail){
-	var indexNumber = i+1;
-	document.getElementById('row-image-'+indexNumber).src = trail.imgMedium;
-	document.getElementById('row-image-'+indexNumber).style.height = 'auto';
-	document.getElementById('row-image-'+indexNumber).style.width = '100vh';
-	document.getElementById('row-title-'+indexNumber).textContent = trail.name;
-	document.getElementById('row-description-'+indexNumber).textContent = trail.summary;
-}
+var mapstyle = [
+  {
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#212121"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.icon",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#757575"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      {
+        "color": "#212121"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#757575"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.country",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#9e9e9e"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.land_parcel",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.locality",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#bdbdbd"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#757575"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#181818"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#616161"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      {
+        "color": "#1b1b1b"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "geometry.fill",
+    "stylers": [
+      {
+        "color": "#2c2c2c"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#8a8a8a"
+      }
+    ]
+  },
+  {
+    "featureType": "road.arterial",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#373737"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#3c3c3c"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway.controlled_access",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#4e4e4e"
+      }
+    ]
+  },
+  {
+    "featureType": "road.local",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#616161"
+      }
+    ]
+  },
+  {
+    "featureType": "transit",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#757575"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#000000"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#3d3d3d"
+      }
+    ]
+  }
+]
